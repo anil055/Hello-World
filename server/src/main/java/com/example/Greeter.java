f13ae419c63e5fce297cf2e8a92863947800242f
ANil Pelluri <anilkumar.pelluri055@gmail.com>
2017-09-04 12:06:40 +0530

    Check style commit java docs

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,11 @@ public class Greeter {
 
   }
 
-  public String greet( final String someone) {
+/**
+*Java doc
+*/
+
+  public final String greet( final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
