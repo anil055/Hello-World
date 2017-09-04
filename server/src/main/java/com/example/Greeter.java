c32ddcf4b28d85faefe70a156605f59b683b95a4
ANil Pelluri <anilkumar.pelluri055@gmail.com>
2017-09-04 12:42:03 +0530

    Fixing analysis issues in unit testing 3

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
   }
 
 /**
-*@peram someone the name of the perameter
+*@param someone the name of the perameter
 *@return greeting string
 */
   public final String greet(final String someone) {
